09ae1b8e1c7568a3331cb80e4c1b0bcfc249f457
NickGuzi <140576874+NickGuzi@users.noreply.github.com>
2023-10-08 23:28:15 -0400

    Added code for TA questionnaire

---
+++ Basic_C#_Programs/DailyReport/Program.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DailyReport
+{
+    class Program
+    {
+        static void Main()
+        {
+            Console.WriteLine("The Tech Academy");
+            Console.Read();
+
+            Console.WriteLine("Student Daily Report");
+            Console.Read();
+
+            Console.WriteLine("What is your name?");
+            string name = Console.ReadLine();
+
+            Console.WriteLine("What course are you on?");
+            string course = Console.ReadLine();
+
+            Console.WriteLine("What page number?");
+            var pageNum = int.Parse(Console.ReadLine());
+
+            Console.WriteLine("Do you need help with anything? Please answer 'True' or 'False'");
+            string answer = Console.ReadLine().Trim().ToLower();
+            if(answer != "true" && answer != "false")
+            {
+                Console.WriteLine("Please enter either 'True' or 'False'");
+                Console.WriteLine("Do you need help with anything? Please answer 'True' or 'False'");
+                string newAnswer = Console.ReadLine().Trim().ToLower();
+            }
+
+            Console.WriteLine("Is there any other feedback you'd like to provide?");
+            string feedback = Console.ReadLine();
+
+            Console.WriteLine("How many hours did you study today?");
+            var studyTime = int.Parse(Console.ReadLine());
+
+            Console.WriteLine("Thank you for your answers. An instructor will respond to this shortly. Have a great day!");
+            Console.Read();
+
+
+
+        }
+    }
+}
