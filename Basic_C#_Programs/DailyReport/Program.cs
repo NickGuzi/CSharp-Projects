fa99549288c97134f8093b2eb68390ce0263b7ba
NickGuzi <140576874+NickGuzi@users.noreply.github.com>
2023-10-08 23:32:40 -0400

    Added comments

--- Basic_C#_Programs/DailyReport/Program.cs
+++ Basic_C#_Programs/DailyReport/Program.cs
@@ -10,37 +10,37 @@ namespace DailyReport
     {
         static void Main()
         {
-            Console.WriteLine("The Tech Academy");
+            Console.WriteLine("The Tech Academy"); // Intro
             Console.Read();
 
-            Console.WriteLine("Student Daily Report");
+            Console.WriteLine("Student Daily Report"); // Second Intro
             Console.Read();
 
-            Console.WriteLine("What is your name?");
+            Console.WriteLine("What is your name?"); // Name Question
             string name = Console.ReadLine();
 
-            Console.WriteLine("What course are you on?");
+            Console.WriteLine("What course are you on?"); // Course QUestion
             string course = Console.ReadLine();
 
-            Console.WriteLine("What page number?");
+            Console.WriteLine("What page number?"); // Page Num Question
             var pageNum = int.Parse(Console.ReadLine());
 
-            Console.WriteLine("Do you need help with anything? Please answer 'True' or 'False'");
+            Console.WriteLine("Do you need help with anything? Please answer 'True' or 'False'"); // Boolean Question
             string answer = Console.ReadLine().Trim().ToLower();
-            if(answer != "true" && answer != "false")
+            if(answer != "true" && answer != "false") // Boolean Question Conditional Operator 
             {
                 Console.WriteLine("Please enter either 'True' or 'False'");
                 Console.WriteLine("Do you need help with anything? Please answer 'True' or 'False'");
                 string newAnswer = Console.ReadLine().Trim().ToLower();
             }
 
-            Console.WriteLine("Is there any other feedback you'd like to provide?");
+            Console.WriteLine("Is there any other feedback you'd like to provide?"); // Feedback question
             string feedback = Console.ReadLine();
 
-            Console.WriteLine("How many hours did you study today?");
+            Console.WriteLine("How many hours did you study today?"); // Hours Studied Question
             var studyTime = int.Parse(Console.ReadLine());
 
-            Console.WriteLine("Thank you for your answers. An instructor will respond to this shortly. Have a great day!");
+            Console.WriteLine("Thank you for your answers. An instructor will respond to this shortly. Have a great day!"); //Program exit
             Console.Read();
 
 
