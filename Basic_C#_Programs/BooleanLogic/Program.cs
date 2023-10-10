9a972b2b1dd2d09d60d6f8d6f282ae739318402e
NickGuzi <140576874+NickGuzi@users.noreply.github.com>
2023-10-09 21:16:11 -0400

    Added code for console app

---
+++ Basic_C#_Programs/BooleanLogic/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BooleanLogic
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Car Insurance Application");
+            Console.WriteLine("Press Enter to continue through steps...");
+            Console.ReadLine();
+
+            Console.WriteLine("What is your age?");
+            var age = int.Parse(Console.ReadLine());
+
+            Console.WriteLine("Have you ever had a DUI?");
+            Console.WriteLine("Please explicitly enter either true or false...");
+            var dui = bool.Parse(Console.ReadLine());
+
+            Console.WriteLine("How many speeding tickets do you have?");
+            var tickets = int.Parse(Console.ReadLine());
+
+            Console.WriteLine("Qualified?");
+            if (age > 15 && dui != true && tickets <= 3)
+            {
+                Console.WriteLine("You are qualified for insurance.");
+            } else
+            {
+                Console.WriteLine("You are not qualified for insurance.");
+            }
+            Console.ReadLine();
+
+
+        }
+    }
+}
