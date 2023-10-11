cb1b588885d9603520c41d8d256e19945328b816
NickGuzi <140576874+NickGuzi@users.noreply.github.com>
2023-10-10 21:21:17 -0400

    Added code for branching console app

---
+++ Basic_C#_Programs/Branching/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Branching
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to Package Express. Please follow the instructions below.");
+            Console.WriteLine("Please press enter to continue through steps...");
+            Console.ReadLine();
+
+            Console.WriteLine("Please enter package weight (in lbs.):");
+            var weight = float.Parse(Console.ReadLine());
+            if (weight > 50)
+            {
+                Console.WriteLine("Package too heavy to be shipped via Package Express. Have a good day.");
+                Console.ReadLine();
+                Environment.Exit(0);
+            }
+
+            Console.WriteLine("Please enter package width (in inches):");
+            var width = float.Parse(Console.ReadLine());
+
+            Console.WriteLine("Please enter package height (in inches):");
+            var height = float.Parse(Console.ReadLine());
+
+            Console.WriteLine("Please enter package length (in inches):");
+            var length = float.Parse(Console.ReadLine());
+
+            if (width + height + length > 50)
+            {
+                Console.WriteLine("Package too big to be shipped via Package Express. Have a good day.");
+                Console.ReadLine();
+                Environment.Exit(0);
+            }
+
+            float quote = ((width * height * length) * weight) / 100;
+
+            Console.WriteLine("Total cost for this package is: " + "$" + quote);
+            Console.ReadLine();
+        }
+    }
+}
