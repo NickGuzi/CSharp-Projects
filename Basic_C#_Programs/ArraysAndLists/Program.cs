58f7b1bcc8556ae9d7b2e42e3aafab02f90e0340
NickGuzi <140576874+NickGuzi@users.noreply.github.com>
2023-10-13 00:00:38 -0400

    Added more code for console app
    This included a few added features to game 2, and the creation of a game 3

--- Basic_C#_Programs/ArraysAndLists/Program.cs
+++ Basic_C#_Programs/ArraysAndLists/Program.cs
@@ -7,6 +7,7 @@ namespace ArraysAndLists
     {
         static void Main()
         {
+            //GAME ONE
             string[] hockeyTeams = { "Bruins", "Capitals", "Maple Leafs", "Hurricanes", "Rangers", "Stars" };
 
             Console.WriteLine("Please select a number between 0 and 5 to get a hockey team...");
@@ -63,12 +64,14 @@ namespace ArraysAndLists
             Console.WriteLine("Thank you for playing our first guessing game, press enter to continue...");
             Console.ReadLine();
 
+            //GAME TWO
+
             List<string> userList = new List<string>();
             int j = 0;
 
             Console.WriteLine("Now we're going to let you create a list...");
 
-            while (j < 1000)
+            while (j < 100)
             {
                 Console.WriteLine("Enter any word of your desire!");
                 string input = Console.ReadLine();
@@ -97,6 +100,75 @@ namespace ArraysAndLists
             }
 
             Console.ReadLine();
+
+            Console.WriteLine("Here is the amount of elements in your list...");
+            Console.WriteLine(userList.Count);
+
+            int indices = userList.Count - 1;
+
+            while (j < 100)
+            {
+                Console.WriteLine("Pick a number between 0 and " + indices + " to see a word's associated position");
+                var pick = int.Parse(Console.ReadLine());
+
+                if (pick > indices)
+                {
+                    Console.WriteLine("You must pick a number between 0 and " + indices);
+                }
+                else
+                {
+                    Console.WriteLine("The word associated with this position is: " + userList[pick]);
+                }
+
+                Console.WriteLine("Want pick another number? Press 1 to pick again or 0 to not...");
+                var listChoice2 = int.Parse(Console.ReadLine());
+
+                if (listChoice2 == 1)
+                {
+                    Console.WriteLine("Okay lets pick another number");
+                }
+                else
+                {
+                    break;
+                }
+
+            }
+
+            //GAME THREE
+
+            Console.WriteLine("Okay this one is going to be real easy...");
+            Console.ReadLine();
+
+            int[] nums = { 66, 42, 19, 48, 33, 99, 1032, 1221, 1456 };
+
+            while (j < 100)
+            {
+                Console.WriteLine("Pick a number between 0 and 8 to see that position's number");
+                var number = int.Parse(Console.ReadLine());
+
+                if (number > 8 || number < 0)
+                {
+                    Console.WriteLine("You must pick a number between 0 and 8...");
+                }
+                else
+                {
+                    Console.WriteLine("The number with this position is: " + nums[number]);
+                }
+
+                Console.WriteLine("Want pick another number? Press 1 to pick again or 0 to not...");
+                var listChoice3 = int.Parse(Console.ReadLine());
+
+                if (listChoice3 == 1)
+                {
+                    Console.WriteLine("Okay lets pick another number");
+                }
+                else
+                {
+                    break;
+                }
+
+            }
+
 
             Console.WriteLine("Thanks for participating, see you next time!");
             Console.ReadLine();
