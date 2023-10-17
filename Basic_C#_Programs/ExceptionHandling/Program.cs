fa5486b7716a808128c79cef2783e6c02da181c1
NickGuzi <140576874+NickGuzi@users.noreply.github.com>
2023-10-17 15:48:53 -0400

    Added code for console app

---
+++ Basic_C#_Programs/ExceptionHandling/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ExceptionHandling
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                List<int> intList = new List<int>() { 8, 6, 7, 5, 3, 0, 9 };
+
+                Console.WriteLine("Please enter a whole number...");
+                var input = int.Parse(Console.ReadLine());
+
+                int result = 0;
+
+                foreach(int i in intList)
+                {
+                    result = i / input;
+                    Console.WriteLine(i + " divided by " + input + " equals " + result + "\n");
+                }
+                Console.ReadLine();
+            }
+            catch (FormatException ex)
+            {
+                Console.WriteLine("Please type a whole number");
+            }
+            catch (DivideByZeroException ex)
+            {
+                Console.WriteLine("Please don't divide by zero");
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+            finally
+            {
+                Console.ReadLine();
+            }
+        }
+    }
+}
