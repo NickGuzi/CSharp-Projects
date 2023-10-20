4f5e77131a40ce4bf5b46f56b5ecfb56aabf192c
NickGuzi <140576874+NickGuzi@users.noreply.github.com>
2023-10-19 22:16:15 -0400

    Created class to perform math functions

---
+++ Basic_C#_Programs/MethodPractice/MathFunc.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MethodPractice
+{
+    public class MathFunc
+    {
+        public int addition(int num1, int num2)
+        {
+            var Result = num1 + num2;
+            return Result;
+        }
+        public int multiply(int num1, int num2)
+        {
+            var Result = num1 * num2;
+            return Result;
+        }
+        public int subtract(int num1, int num2)
+        {
+            var Result = num1 - num2;
+            return Result;
+        }
+    }
+}
