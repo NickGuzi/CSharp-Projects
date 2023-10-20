af0824dba84f74ea2a8f593cc725f4d20540582b
NickGuzi <140576874+NickGuzi@users.noreply.github.com>
2023-10-19 23:12:23 -0400

    Added code that utilized MathFunc class

---
+++ Basic_C#_Programs/MethodPractice2/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MethodPractice2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            //Int equation
+            int Result;
+            MathFunc func = new MathFunc();
+            Console.WriteLine("Please enter a whole number");
+            var input1 = int.Parse(Console.ReadLine());
+
+            Result = func.funcOne(input1);
+
+            Console.WriteLine("The total of the first equation is: " + Result);
+            Console.ReadLine();
+
+            //Decimal Equation
+            int Total;
+            MathFunc func2 = new MathFunc();
+            Console.WriteLine("Please enter a decimal number");
+            var input2 = decimal.Parse(Console.ReadLine());
+
+            Total = func2.funcOne(input2);
+
+            Console.WriteLine("The total of the second equation is: " + Total);
+            Console.ReadLine();
+
+            //String Equation
+            int Conclusion;
+            MathFunc func3 = new MathFunc();
+            Console.WriteLine("Please enter a number");
+            string input3 = Console.ReadLine();
+
+            Conclusion = func3.funcOne(input3);
+
+            Console.WriteLine("The total of the third equation is: " + Conclusion);
+            Console.ReadLine();
+        }
+    }
+}
