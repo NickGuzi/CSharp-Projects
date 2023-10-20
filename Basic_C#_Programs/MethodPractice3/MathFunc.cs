25f499af208217a61a2421411463b2d3e893ccc1
NickGuzi <140576874+NickGuzi@users.noreply.github.com>
2023-10-19 23:36:32 -0400

    Created class for math operation

---
+++ Basic_C#_Programs/MethodPractice3/MathFunc.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MethodPractice3
+{
+    class MathFunc
+    {
+        public int twoInts(int int1, int int2 = 0)
+        {
+            int Result;
+            Result = int1 + int2;
+            return Result;
+        }
+    }
+}
