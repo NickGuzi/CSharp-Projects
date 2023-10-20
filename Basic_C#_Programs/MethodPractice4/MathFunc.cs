e5c5f19e60cfffc5d8d5ea97659a9708878556a2
NickGuzi <140576874+NickGuzi@users.noreply.github.com>
2023-10-20 00:04:35 -0400

    Created class and method

---
+++ Basic_C#_Programs/MethodPractice4/MathFunc.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MethodPractice4
+{
+    public class MathFunc
+    {
+        public void funcOne(int int1, int int2, out int answer)
+        {
+            answer = int1 * 2;
+
+            Console.WriteLine("This is the second number you entered: " + int2);
+        }
+    }
+}
