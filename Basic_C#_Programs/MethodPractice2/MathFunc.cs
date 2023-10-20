aef711524de268babfa3446a2b46ed60da588230
NickGuzi <140576874+NickGuzi@users.noreply.github.com>
2023-10-19 23:12:09 -0400

    Created class for math operations

---
+++ Basic_C#_Programs/MethodPractice2/MathFunc.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MethodPractice2
+{
+    public class MathFunc
+    {
+        public int funcOne(int num1)
+        {
+            int num2 = 6;
+            var Result = num1 * num2;
+            return Result;
+        }
+
+        public int funcOne(decimal num1)
+        {
+            decimal num2 = 3.00m;
+            var Total = Convert.ToInt32(num1 / num2);
+            return Total;
+        }
+
+        public int funcOne(string string1)
+        {
+            int num3 = 4;
+           int num4 = Convert.ToInt32(string1);
+            int Answer = num4 - num3;
+            return Answer;
+        }
+    }
+}
