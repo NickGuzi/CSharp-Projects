4eea9bb60bc44b5cc4ca638c3397917084d3e7f0
NickGuzi <140576874+NickGuzi@users.noreply.github.com>
2023-10-20 00:04:54 -0400

    Added code to utilize MathFunc

---
+++ Basic_C#_Programs/MethodPractice4/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MethodPractice4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            MathFunc func = new MathFunc();
+            int answer;
+
+            Console.WriteLine("Please enter a whole number: ");
+            var input1 = int.Parse(Console.ReadLine());
+            Console.WriteLine("Please enter a second whole number: ");
+            var input2 = int.Parse(Console.ReadLine());
+
+            func.funcOne(input1, input2, out answer);
+            Console.WriteLine(input1 + " times 2 equals: " + answer);
+            Console.ReadLine();
+
+        }
+    }
+}
