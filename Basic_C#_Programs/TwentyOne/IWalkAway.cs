4728af247a669d38954fc857aa662bb3a45908ce
NickGuzi <140576874+NickGuzi@users.noreply.github.com>
2023-10-23 01:02:22 -0400

    Added interface IWalkAway and Implemented accordingly in inherited class TwentyOneGame

---
+++ Basic_C#_Programs/TwentyOne/IWalkAway.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TwentyOne
+{
+    interface IWalkAway
+    {
+        //No need to define interface methods as public 
+        void WalkAway(Player player);
+    }
+}
