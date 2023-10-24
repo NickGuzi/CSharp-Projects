def85df554ff6e377e9440b2425c9b3c1bb4f507
NickGuzi <140576874+NickGuzi@users.noreply.github.com>
2023-10-23 23:51:53 -0400

    Overloaded comparison operator to check whether or not employees are same person or not

--- Basic_C#_Programs/MethodsPractice5/Employee.cs
+++ Basic_C#_Programs/MethodsPractice5/Employee.cs
@@ -12,13 +12,23 @@ namespace MethodsPractice5
 
         public override void SayName()
         {
-            Console.WriteLine("Employees: ");
+            Console.WriteLine("Employee: ");
             base.SayName();
         }
 
         public void Quit()
         {
             Console.WriteLine("Enter name of Employee who quit here: ");
+            string quitter = Console.ReadLine();
+        }
+
+        public static bool operator== (Employee emp1, Employee emp2)
+        {
+            return emp1.Id == emp2.Id;
+        }
+        public static bool operator !=(Employee emp1, Employee emp2)
+        {
+            return emp1.Id != emp2.Id;
         }
     }
 }
--- Basic_C#_Programs/MethodsPractice5/Program.cs
+++ Basic_C#_Programs/MethodsPractice5/Program.cs
@@ -13,16 +13,22 @@ namespace MethodsPractice5
             Employee name = new Employee();
             name.FirstName = "Sample";
             name.LastName = "Student";
+            name.Id = 1;
             name.SayName();
+
+            Employee name2 = new Employee();
+            name2.FirstName = "Nick";
+            name2.LastName = "Guzi";
+            name2.Id = 2;
+            name2.SayName();
+            Console.ReadLine();
+
+            Console.WriteLine("Are employee 1 and employee 2 the same person? \n" + (name == name2));
             Console.ReadLine();
 
             IQuittable quit = new Employee();
             quit.Quit();
             Console.ReadLine();
-
-
-
-
 
         }
     }
