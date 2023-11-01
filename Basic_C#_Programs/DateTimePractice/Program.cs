1678f1288f97b1166dbed51224f76b40e57cb82b
NickGuzi <140576874+NickGuzi@users.noreply.github.com>
2023-10-31 23:55:05 -0400

    Created simple console app program to practice with DateTime

---
+++ Basic_C#_Programs/DateTimePractice/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DateTimePractice
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("The current date & time is: " + DateTime.Now);
+            Console.WriteLine("Please enter a whole number: ");
+            var input = int.Parse(Console.ReadLine());
+            Console.WriteLine("In {0} hours it will be " + DateTime.Now.AddHours(input), input);
+            Console.ReadLine();
+
+        }
+    }
+}
